31e202c6e4d566cf4d269bfd62dd15903f0e1aa8
tico7202 <tico7202@naver.com>
2023-12-27 19:55:58 +0900

    2023.12.27 포폴 작업

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] GameObject player;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        checkPlayer();
+    }
+    private void checkPlayer()
+    {
+        if( player != null)
+        {
+            Vector3 pos = player.transform.position;
+            pos.z = -10;
+            transform.position = pos;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,14 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{ 
     Rigidbody2D rigid;
     Camera mainCam;
+    BoxCollider2D box;
+
+    private float verticalVelocity;
+    private bool checkjumping = false;
+    private bool isGround = false;
 
     [Header("플레이어 관련")]
     [SerializeField] private float moveSpeed = 5.0f;
@@ -14,12 +19,13 @@ public class Player : MonoBehaviour
 
     [Header("중력 및 점프")]
     private float gravity = 9.81f;
-    [SerializeField]private bool isGround = false;
+    [SerializeField] private float jumpForce = 5f;
     
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
     }
 
     void Start()
@@ -30,17 +36,70 @@ public class Player : MonoBehaviour
     void Update()
     {
         moving();
-        checkGravity();
+        checkjump();
+        jumping();
+        checkGround();
+        turning();
     }
+    private void turning()
+    {
+        if (moveDir.x > 0 && transform.localScale.x < 1)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+        else if(moveDir.x < 0 && transform.localScale.x > -1)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
     private void moving()
     {
         moveDir.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
         moveDir.y = rigid.velocity.y;
         rigid.velocity = moveDir;
     }
-    
-    private void checkGravity()
-    {
 
+    private void checkGround()
+    {
+        isGround = false;
+        RaycastHit2D hit = Physics2D.BoxCast(box.bounds.center, box.bounds.size, 0f,
+            Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+        if(hit.transform != null)
+        {
+            isGround = true;
+            verticalVelocity = 0;
+        }
+    }
+
+    private void checkjump()
+    {
+        if (Input.GetKey(KeyCode.Space)&& isGround == true)
+        {
+            checkjumping = true;
+            jumping();
+        }
+    }
+
+    private void jumping()
+    {
+        if (isGround == false)
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+            if (verticalVelocity < -10)
+            {
+                verticalVelocity = -10;
+            } 
+        }
+        if(checkjumping == true)
+        {
+            checkjumping = false;
+            verticalVelocity = jumpForce;
+        }
+        rigid.velocity = new Vector2(rigid.velocity.x, verticalVelocity);
     }
 }
